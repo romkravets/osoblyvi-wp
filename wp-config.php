24a7fdcdd6ca14b4ec43dafca2dfd5fb3e4a210f
romkravets <romann.kravets@gmail.com>
2020-08-08 11:15:40 +0300

    start open server

--- wp-config.php
+++ wp-config.php
@@ -23,16 +23,16 @@
 define( 'DB_NAME', 'db' );
 
 /** Имя пользователя MySQL */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'root' );
 
 /** Пароль к базе данных MySQL */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'root' );
 
 /** Имя сервера MySQL */
 define( 'DB_HOST', 'localhost' );
 
 /** Кодировка базы данных для создания таблиц. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** Схема сопоставления. Не меняйте, если не уверены. */
 define( 'DB_COLLATE', '' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'впишите сюда уникальную фразу' );
-define( 'SECURE_AUTH_KEY',  'впишите сюда уникальную фразу' );
-define( 'LOGGED_IN_KEY',    'впишите сюда уникальную фразу' );
-define( 'NONCE_KEY',        'впишите сюда уникальную фразу' );
-define( 'AUTH_SALT',        'впишите сюда уникальную фразу' );
-define( 'SECURE_AUTH_SALT', 'впишите сюда уникальную фразу' );
-define( 'LOGGED_IN_SALT',   'впишите сюда уникальную фразу' );
-define( 'NONCE_SALT',       'впишите сюда уникальную фразу' );
+define( 'AUTH_KEY',         '+6v(#*rK)1h*$j8JhWLPiYN+Jp6[L^Ovf&q5ln_@LTB3%yXO~B*Q@9PQz8;,K)NP' );
+define( 'SECURE_AUTH_KEY',  'P^CNI*Rx3/$Ix}83!~6:gFg]B`EAL*$UiskZ._+jJc#=m=vM_]j!/-XI|,/r<]ZJ' );
+define( 'LOGGED_IN_KEY',    'i[1>|WAA5qUx*v& yoRWo5$~,/weJ1OZ0EWc]p/rZ`hSWeY#r[`)`]c9$5Ni#k|M' );
+define( 'NONCE_KEY',        ' rA%9p)))$L;DwnOoi#(p/g>8+8j%Dc;8bdUHmhJ6X7T7;JM;!*ey6*@rY=ov|}&' );
+define( 'AUTH_SALT',        'Y/~N*ARr~QGO?<0:NBK0pn%7=AMN{E2~:aj-w+s=M]+nr:6gu-f$VX(n`[(}M}G_' );
+define( 'SECURE_AUTH_SALT', 'uC8WJ`gEPRsb~#s!Gmd#,<NGKC8hW;.wwvB`F$^6$CpQIO*f|Osp2S0fAMj]=LjA' );
+define( 'LOGGED_IN_SALT',   'IN?9]9r~?H1ri HcW^+<wG5b}K-hLA<T!WL#KLMCXesN-+?H$a/q1$rRn.OPM_JS' );
+define( 'NONCE_SALT',       '_K#`*Qu 9*NTMG5HS~{m&Y5M5L-PSY`n^)KcAK i=JRI8!)#DdT7sQePUGJ{o>zn' );
 
 /**#@-*/
 
