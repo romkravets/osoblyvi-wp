5ad09b3f8bb246264caa16c2043eb4714d7ad791
romkravets <romann.kravets@gmail.com>
2020-08-25 15:06:38 +0300

    blog articles

--- wp-content/themes/hestia-child/functions.php
+++ wp-content/themes/hestia-child/functions.php
@@ -28,3 +28,37 @@ function hestia_child_get_lite_options() {
 add_action( 'after_switch_theme', 'hestia_child_get_lite_options' );
 
 wp_enqueue_script( 'jquery' );
+
+function hestia_excerpt_more_new( $more ) {
+	global $post;
+	if ( ( ( 'page' === get_option( 'show_on_front' ) ) ) || is_single() || is_archive() || is_home() ) {
+		return '<a class="moretag" href="' . esc_url( get_permalink( $post->ID ) ) . '"> ' . esc_html__( 'Читати більше...', 'hestia-pro' ) . '</a>';
+	}
+	return $more;
+}
+add_filter( 'excerpt_more', 'hestia_excerpt_more_new', 20 );
+
+add_filter( 'hestia_single_post_meta','child_hestia_single_post_meta_function' );
+
+function child_hestia_single_post_meta_function() {
+    return sprintf(
+        /* translators: %1$s is Author name wrapped, %2$s is Date*/
+        esc_html__( 'Опубліковано: %1$s публікація: %2$s', 'hestia-pro' ),
+        /* translators: %1$s is Author name, %2$s is Author link*/
+        sprintf(
+            '<a href="%2$s" class="vcard author"><strong class="fn">%1$s</strong></a>',
+            esc_html( get_the_author_meta( 'display_name', get_post_field ('post_author') ) ),
+            esc_url( get_author_posts_url( get_post_field ('post_author') ) )
+        ),
+        /* translators: %s is Date */
+        sprintf(
+            '<time class="date updated published" datetime="%2$s">%1$s</time>',
+            esc_html( get_the_time( get_option( 'date_format' ) ) ), esc_html( get_the_date( DATE_W3C ) )
+        )
+    );
+}
+
+function change_related_posts_title() {
+   return 'Більше новин';
+}
+add_filter( 'hestia_related_posts_title', 'change_related_posts_title' );
